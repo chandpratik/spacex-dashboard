30277025704d4f74343b58e038ab92bd33ccf9ab
pratikal1 <chandpratikal1@gmail.com>
2021-05-31 12:11:19 +0530

    feat: add dummy pagination component

--- src/containers/Frame/index.js
+++ src/containers/Frame/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Pagination from 'react-js-pagination';
 
 import { Filter, Table } from '../../components';
 
@@ -7,6 +8,20 @@ export const Frame = () => {
     <div className="w-3/4 mx-auto">
       <Filter />
       <Table />
+      <Pagination
+        activePage={1}
+        itemsCountPerPage={10}
+        totalItemsCount={200}
+        pageRangeDisplayed={5}
+        onChange={console.log('changed')}
+        prevPageText={'<'}
+        nextPageText={'>'}
+        hideFirstLastPages={true}
+        innerClass={'inline-flex mt-4 shadow-sm'}
+        itemClass={
+          ' text-gray-600 relative inline-flex items-center px-3 py-1 border text-sm font-sm '
+        }
+      />
     </div>
   );
 };
