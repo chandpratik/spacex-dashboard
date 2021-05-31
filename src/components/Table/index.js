1d0b12df2c7a629dda393c5dfc3462708f52f940
pratikal1 <chandpratikal1@gmail.com>
2021-05-31 11:11:10 +0530

    fix: missing imports

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -2,36 +2,19 @@ import React from 'react';
 
 import { useFetch } from '../../hooks/useFetch';
 import { Head } from './Head';
+import { Row } from './Row';
 
 export const Table = () => {
   const { response } = useFetch(
     'https://api.spacexdata.com/v3/launches?limit=12'
   );
 
-  console.log(response);
-
   return (
-    <table className="w-full mt-4 table-auto">
+    <table className="w-full h-100 mt-4 table-auto border-2">
       <Head />
       <tbody>
         {response.map((launch) => (
-          <tr key={launch.flight_number}>
-            <td>{launch.flight_number}</td>
-            <td>{launch.launch_date_utc}</td>
-            <td>{launch.launch_site.site_id.replace(/_/g, ' ')}</td>
-            <td>{launch.mission_name}</td>
-            <td className="text-center">
-              {launch.rocket.second_stage.payloads[0].orbit}
-            </td>
-            <td className="text-center">
-              {launch.upcoming
-                ? 'Upcoming'
-                : launch.launch_success
-                ? 'Success'
-                : 'Failed'}
-            </td>
-            <td className="text-center">{launch.rocket.rocket_name}</td>
-          </tr>
+          <Row key={launch.flight_number} {...launch} />
         ))}
       </tbody>
     </table>
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Filter } from './Filter';
 
 export * from './Header';
 export * from './Table';
+export * from './Badge';
