db9281d7cc7b2c6d9b16c49e3c57bf6b72dde495
pratikal1 <chandpratikal1@gmail.com>
2021-06-03 02:05:03 +0530

    refact: convert into compound component

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,55 +1,39 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { ChevronDownIcon, FilterIcon } from '@heroicons/react/outline';
+import { useTableContext } from '../../context';
 
-import { filterlist } from '../../constants/filterlist';
-import { useTableContext, usePaginationContext } from '../../context';
-
-export function Filter() {
-  const [open, setOpen] = useState(false);
-
-  const { selection, setSelection, setQueryFilter } = useTableContext();
-  const { setOffset, setActiveNumber } = usePaginationContext();
-
-  const toggle = () => setOpen(!open);
-
-  const handleOnClick = (filter) => {
-    setOpen(!open);
-    setSelection(filter.value);
-    setQueryFilter(filter.queryParams);
-    setOffset(0);
-    setActiveNumber(1);
-  };
-
+export function Filter({ children }) {
   return (
     <div className="flex-column w-52 h-8 mt-4 ml-auto opacity-100 ">
-      <div
-        tabIndex={0}
-        role="button"
-        onKeyPress={() => toggle(!open)}
-        onClick={() => toggle(!open)}
-        className="flex justify-around "
-      >
-        <FilterIcon className="inline-block h-4 w-4 mt-1 " />
-
-        <div>
-          <span className="font-semibold align-top text-gray-600 ">
-            {selection}
-          </span>
-          <ChevronDownIcon className="inline-block h-4 w-4 ml-2 " />
-        </div>
-      </div>
-      {open && (
-        <ul className="py-2 border-2 rounded relative z-20 bg-white ">
-          {filterlist.map((filter) => (
-            <li key={filter.id} className="py-1 bg-white hover:bg-gray-100">
-              <button onClick={() => handleOnClick(filter)}>
-                <span className="font-sm pl-4">{filter.value}</span>
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
+      {children}
     </div>
   );
 }
+
+Filter.Frame = function FilterFrame({ children, action }) {
+  const { isDropdownOpen, setIsDropdownOpen } = useTableContext();
+  const handleClick = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+  return (
+    <div
+      tabIndex={0}
+      role="button"
+      onKeyPress={handleClick}
+      onClick={handleClick}
+      className="flex justify-around"
+    >
+      {children}
+    </div>
+  );
+};
+
+Filter.Innerframe = function FilterInnerframe({ children }) {
+  return <div>{children}</div>;
+};
+
+Filter.Dropdown = function FilterDropdown({ children }) {
+  return (
+    <ul className="py-2 border-2 rounded relative z-20 bg-white">{children}</ul>
+  );
+};
