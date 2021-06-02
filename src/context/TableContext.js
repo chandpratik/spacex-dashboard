fb4b1b3616cc6a6da580252f458958f0e9e4f336
pratikal1 <chandpratikal1@gmail.com>
2021-06-03 02:03:33 +0530

    add isDropdownOpen value to context

--- src/context/TableContext.js
+++ src/context/TableContext.js
@@ -5,12 +5,15 @@ export const TableContext = createContext(null);
 export const TableContextProvider = ({ children }) => {
   const [selection, setSelection] = useState('All Launches');
   const [queryFilter, setQueryFilter] = useState('/?limit=12');
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const value = {
     selection,
     setSelection,
     queryFilter,
     setQueryFilter,
+    isDropdownOpen,
+    setIsDropdownOpen,
   };
   return (
     <TableContext.Provider value={value}>{children}</TableContext.Provider>
