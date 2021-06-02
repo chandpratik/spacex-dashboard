78e0e257abcd5815833c8fa5503d87578674ab67
pratikal1 <chandpratikal1@gmail.com>
2021-06-02 22:50:17 +0530

    feat: add modal for each row

--- src/components/Table/Row.js
+++ src/components/Table/Row.js
@@ -7,16 +7,21 @@ import {
   setLaunchStatus,
 } from '../../utils/table.utils';
 import { Badge } from '../Badge';
+import { Modal } from '../Modal';
 
-export const Row = ({
-  flight_number,
-  launch_date_utc,
-  launch_site,
-  mission_name,
-  rocket,
-  upcoming,
-  launch_success,
-}) => {
+import { useModalContext } from '../../context';
+
+export const Row = (props) => {
+  const { showModal, setShowModal } = useModalContext();
+  const {
+    flight_number,
+    launch_date_utc,
+    launch_site,
+    mission_name,
+    rocket,
+    upcoming,
+    launch_success,
+  } = props;
   const launch_date = formatLaunchDate(launch_success, launch_date_utc);
   const no = appendZero(flight_number);
   const launchStatus = setLaunchStatus(upcoming, launch_success);
@@ -30,22 +35,42 @@ export const Row = ({
       : 'text-red-800';
 
   return (
-    <tr>
-      <td className="text-sm text-center font-normal pr-3">{no}</td>
-      <td className="text-sm font-normal">{launch_date}</td>
-      <td className="text-sm font-normal">{launch_site.site_name}</td>
-      <td className="text-sm font-normal">{mission_name}</td>
-      <td className="text-sm text-center font-normal">
-        {rocket.second_stage.payloads[0].orbit}
-      </td>
-      <td className="text-sm text-center font-normal">
-        <Badge
-          status={launchStatus}
-          badgeColour={badgeColour}
-          badgeTextColour={textColour}
+    <>
+      <tr
+        className="cursor-pointer relative h-9.5"
+        onClick={() => setShowModal(!showModal)}
+      >
+        <td className="text-sm text-center font-normal ">{no}</td>
+        <td className="text-sm font-normal ">{launch_date}</td>
+        <td className="text-sm font-normal ">{launch_site.site_name}</td>
+        <td className="text-sm font-normal ">{mission_name}</td>
+        <td className="text-sm text-center font-normal ">
+          {rocket.second_stage.payloads[0].orbit}
+        </td>
+        <td className="text-sm text-center font-normal ">
+          <Badge
+            status={launchStatus}
+            badgeColour={badgeColour}
+            badgeTextColour={textColour}
+          />
+        </td>
+        <td className="text-sm text-center font-normal ">
+          {rocket.rocket_name}
+        </td>
+      </tr>
+      {showModal && (
+        <Modal
+          {...props}
+          date={launch_date}
+          badge={
+            <Badge
+              status={launchStatus}
+              badgeColour={badgeColour}
+              badgeTextColour={textColour}
+            />
+          }
         />
-      </td>
-      <td className="text-sm text-center font-normal">{rocket.rocket_name}</td>
-    </tr>
+      )}
+    </>
   );
 };
