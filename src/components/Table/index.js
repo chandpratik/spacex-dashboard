2b6a643f0b7bc77b7457ab754564f042a0b24a22
pratikal1 <chandpratikal1@gmail.com>
2021-06-02 22:52:09 +0530

    add Spinner,EmptyState component and modalcontext

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 import { Head } from './Head';
 import { Row } from './Row';
+import { Spinner } from '../Spinner';
+import { EmptyState } from '../EmptyState';
 
 import { useFetch } from '../../hooks/useFetch';
 import { useTableContext, usePaginationContext } from '../../context';
+import { ModalContextProvider } from '../../context';
 
 export const Table = () => {
   const { queryFilter } = useTableContext();
@@ -15,22 +18,26 @@ export const Table = () => {
   const { response, loading } = useFetch(apiUrl);
 
   return (
-    <table className="w-full h-100 mt-4 table-fixed border-2">
-      <Head />
-      {loading ? (
-        'Loading....'
-      ) : response.length < 1 ? (
-        'No results for specified filter'
-      ) : (
-        <tbody>
-          {response.map((launch) => (
-            <Row
-              key={`${launch.flight_number}${launch.launch_date_unix}`}
-              {...launch}
-            />
-          ))}
-        </tbody>
-      )}
-    </table>
+    <div className="border-2 min-h-500 rounded-md mt-4">
+      <table className="w-full table-fixed relative">
+        <Head />
+        {loading ? (
+          <Spinner />
+        ) : response.length < 1 ? (
+          <EmptyState />
+        ) : (
+          <tbody>
+            {response.map((launch) => (
+              <ModalContextProvider>
+                <Row
+                  key={`${launch.flight_number} ${launch.launch_date_unix}`}
+                  {...launch}
+                />
+              </ModalContextProvider>
+            ))}
+          </tbody>
+        )}
+      </table>
+    </div>
   );
 };
