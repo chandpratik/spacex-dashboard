d3bde0ab6ae5d4eae9e806aa9788f2a851b67bbe
pratikal1 <chandpratikal1@gmail.com>
2021-06-05 00:22:51 +0530

    refact: remove console.log

--- src/components/ErrorState/index.js
+++ src/components/ErrorState/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export const ErrorState = ({ error }) => {
-  console.log(error);
   return (
     <p className="text-center text-gray-500 absolute top-135/100 left-38/100">
       {error}
