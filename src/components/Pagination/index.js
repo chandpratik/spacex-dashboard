fd7c01f25837c80e65edf93d119c77b0865f5b34
pratikal1 <chandpratikal1@gmail.com>
2021-06-05 00:38:02 +0530

    add comments

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -15,17 +15,17 @@ export const Paginate = ({ totalItemsCount }) => {
       <Pagination
         activePage={activeNumber}
         itemsCountPerPage={12}
-        totalItemsCount={totalItemsCount}
-        pageRangeDisplayed={10}
+        totalItemsCount={totalItemsCount} //Total count of items which you are going to display
+        pageRangeDisplayed={10} //Range of pages in paginator, exclude navigation blocks (prev, next, first, last pages)
         prevPageText={'<'}
         nextPageText={'>'}
         hideFirstLastPages={true}
-        innerClass={'inline-flex mt-4 shadow-sm'}
+        innerClass={'inline-flex mt-4 shadow-sm'} //Class name of <ul> tag
         itemClass={
           'text-gray-600 inline-flex items-center px-3 py-1 border text-sm font-sm cursor-pointer'
-        }
-        activeClass={'bg-gray-300'}
-        disabledClass={'cursor-default opacity-20'}
+        } //Default class of the <li> tag
+        activeClass={'bg-gray-300'} //Class name of active <li> tag
+        disabledClass={'cursor-default opacity-20'} //Class name of the first, previous, next and last <li> tags when disabled
         onChange={handleChange}
       />
     </div>
